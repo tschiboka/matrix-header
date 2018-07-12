6120a7b98a79b1432eafc7091cade8517b3464fe
Tschiboka <tibi.aki.tivadar@gmail.com>
2018-07-12 23:49:39 +0100

    create a table with letters, not random letters yet

--- matrix-header.js
+++ matrix-header.js
@@ -1,4 +1,16 @@
-// create matrix table cells arg1 row number, arg2 cell num
-function createMatrix(r,c) {
-    alert("Hey");
+// create matrix table cells arg1 row number, arg2 cell num and render random letters to them
+function createMatrix(row,col,tableId) {    
+    let tableBody = document.createElement("tbody");
+    for (r = 0; r < row; r++) {
+        let tableRow = document.createElement("tr");
+        for (c = 0; c < col; c++) {
+            let tableCell = document.createElement("td");
+            tableCell.id = `cell${r}_${c}`; // eg.: cell1_5; starts 0 so it won't collide with arrays later
+            tableCell.innerHTML = "A"; // temporary, just for checking how it looks
+            tableRow.appendChild(tableCell);            
+        } // end of inner for
+        tableBody.appendChild(tableRow);    
+    } // end of outer for
+    document.getElementById(tableId).appendChild(tableBody);
+    console.log(document.getElementById("letters"));
 } // end of createMatrix
