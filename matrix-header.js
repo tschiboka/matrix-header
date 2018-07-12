31c44b0fd72cabb40dfb66fd0f8d84fe09ce32c7
Tschiboka <tibi.aki.tivadar@gmail.com>
2018-07-13 00:02:39 +0100

    now the letters are random

--- matrix-header.js
+++ matrix-header.js
@@ -1,12 +1,14 @@
 // create matrix table cells arg1 row number, arg2 cell num and render random letters to them
 function createMatrix(row,col,tableId) {    
+    const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!?<>-()*",
+          randomLetter = () => letters[Math.floor(Math.random()*letters.length)];    
     let tableBody = document.createElement("tbody");
     for (r = 0; r < row; r++) {
         let tableRow = document.createElement("tr");
         for (c = 0; c < col; c++) {
             let tableCell = document.createElement("td");
             tableCell.id = `cell${r}_${c}`; // eg.: cell1_5; starts 0 so it won't collide with arrays later
-            tableCell.innerHTML = "A"; // temporary, just for checking how it looks
+            tableCell.innerHTML = randomLetter(); // temporary, just for checking how it looks
             tableRow.appendChild(tableCell);            
         } // end of inner for
         tableBody.appendChild(tableRow);    
