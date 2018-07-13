f5a95792316801a0430f509fc6ee04c31308d8ac
Tschiboka <tibi.aki.tivadar@gmail.com>
2018-07-13 01:21:13 +0100

    responsive header size

--- matrix-header.js
+++ matrix-header.js
@@ -1,18 +1,19 @@
 // create matrix table cells arg1 row number, arg2 cell num and render random letters to them
-function createMatrix(row,col,tableId) {    
+function createMatrix(row,col,tableId) {        
     const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!?<>-()*",
-          randomLetter = () => letters[Math.floor(Math.random()*letters.length)];    
+          randomLetter = () => letters[Math.floor(Math.random()*letters.length)],
+          cellSize = Math.floor(window.getComputedStyle(document.getElementsByTagName("header")[0],null).width.match(/\d+/)[0] / col); 
     let tableBody = document.createElement("tbody");
     for (r = 0; r < row; r++) {
         let tableRow = document.createElement("tr");
         for (c = 0; c < col; c++) {
             let tableCell = document.createElement("td");
-            tableCell.id = `cell${r}_${c}`; // eg.: cell1_5; starts 0 so it won't collide with arrays later
-            tableCell.innerHTML = randomLetter(); // temporary, just for checking how it looks
+            tableCell.id = `cell${r}_${c}`; // eg.: cell1_5; starts 0 so it won't collide with arrays later            
+            tableCell.innerHTML = randomLetter();                                    
+            tableCell.style.width = cellSize + "px"; // responsive size settings    
             tableRow.appendChild(tableCell);            
         } // end of inner for
         tableBody.appendChild(tableRow);    
-    } // end of outer for
-    document.getElementById(tableId).appendChild(tableBody);
-    console.log(document.getElementById("letters"));
+    } // end of outer for    
+    document.getElementById(tableId).appendChild(tableBody);    
 } // end of createMatrix
